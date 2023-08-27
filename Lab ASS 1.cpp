807e3ee08118e207c46d870b33228e7b3a8dbf0f
Tooba Masood <toobamasood786@gmail.com>
2023-08-27 17:59:53 +0500

    comit

---
+++ Lab ASS 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1=10;
+	float num2=15.5;
+	char ch1='b';
+	char ch2='a';
+	cout<<"The integer value ="<<num1<<endl;
+	cout<<"The floating point value="<<num2<<endl;
+	cout<<"The Character type value="<<ch1<<endl;
+	cout<<"The character type value2="<<ch2;
+	return 0;
+}
